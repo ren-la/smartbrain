1659e36e7c63c9c13b0e23f2b0b133d8649f8e89
renla <eslbauer.verena@gmail.com>
2022-04-11 11:13:15 +0200

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation.js'
 import './App.css';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-
+        <Navigation />
       </div>
     );
   }
