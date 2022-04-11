4bbe8f8ba4d510be94647b3f038f83fd936cc8df
renla <eslbauer.verena@gmail.com>
2022-04-11 22:41:22 +0200

    Add bounding box

--- src/App.js
+++ src/App.js
@@ -30,7 +30,25 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width * (1 - clarifaiFace.right_col),
+      bottomRow: height * (1 - clarifaiFace.bottom_row),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box});
   }
 
   onInputChange = (event) => {
@@ -42,14 +60,8 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-
-      }
-    );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render () {
@@ -64,7 +76,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
